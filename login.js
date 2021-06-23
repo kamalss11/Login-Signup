7d3b2323edd1d4595699e9453cc525a20f677757
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-23 18:36:35 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,6 @@
+function toggleform(){
+    container = document.querySelector(".container");
+    container.classList.toggle("active");
+    section = document.querySelector("section");
+    section.classList.toggle("active");
+}
